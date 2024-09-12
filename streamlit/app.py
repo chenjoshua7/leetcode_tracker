e01182d4059c70ef673e4b7c43707093059879b0
Joshua Chen <chen.joshua98@gmail.com>
2024-09-12 16:46:00 -0400

    aestheic updates

--- streamlit/app.py
+++ streamlit/app.py
@@ -12,25 +12,48 @@ df = querier.query("SELECT * FROM daily_problems ORDER BY date DESC", col_names 
 
 # Sidebar navigation
 with st.sidebar:
-    st.markdown("<h1 style:'padding:30px;'>Navigation</h1>", unsafe_allow_html = True)
+    st.markdown("<h1 style='text-align: center; padding: 20px 0;'>Navigation</h1>", unsafe_allow_html=True)
     
-    if st.button("🏠  Portfolio"):
-        webbrowser.open_new_tab("https://chenjoshua7.github.io")
-    
-    if st.button("🐙  GitHub"):
-        webbrowser.open_new_tab("https://www.github.com/chenjoshua7")
-    
-    if st.button("🔗  LinkedIn"):
-        webbrowser.open_new_tab("https://www.linkedin.com/in/chenjoshua7")
+    # Create columns for better alignment of buttons
+    col1 = st.columns(1)
+    with col1[0]:
+        if st.button("🏠  Portfolio", use_container_width=True):
+            webbrowser.open_new_tab("https://chenjoshua7.github.io")
+        
+        if st.button("🐙  GitHub", use_container_width=True):
+            webbrowser.open_new_tab("https://www.github.com/chenjoshua7")
+        
+        if st.button("🔗  LinkedIn", use_container_width=True):
+            webbrowser.open_new_tab("https://www.linkedin.com/in/chenjoshua7")
 
-    st.write("""
-    **Email**: chen.joshua98@gmail.com
-    """)
+    st.markdown("<p style='margin-top: 10px;'>Email: <a href='mailto:chen.joshua98@gmail.com'>chen.joshua98@gmail.com</a></p>", unsafe_allow_html=True)
+    st.markdown("<p style='margin-top: 0px;'>Repo: <a href='https://github.com/chenjoshua7/leetcode_tracker'>/chenjoshua7/leetcode_tracker</a></p>", unsafe_allow_html=True)
 
+    st.markdown("<h2>About the Project</h2>", unsafe_allow_html=True)
+    st.markdown("")
 # Title and description
-st.markdown("<h1 style='text-align: center;'>LeetCode Daily Challenge Progress</h1>", unsafe_allow_html=True)
-st.markdown("<h4 'style='text-align: center;'>Joshua Chen</h4>", unsafe_allow_html=True)
-st.markdown("<p style='text-align: left;'>Check out my progress with LeetCode Daily Challenges</p>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; padding-top:-10px; padding-bottom: 20px;'>LeetCode Daily Challenge Progress</h1>", unsafe_allow_html=True)
+st.markdown("<h4 style='text-align: center; padding-bottom: 10px;'>Joshua Chen</h4>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: justify; font-size: 18px; padding-left: 30px; padding-right: 30px;'>Check out my progress with LeetCode Daily Challenges, where I solve algorithmic problems to hone my problem-solving skills.</p>", unsafe_allow_html=True)
+
+c1, c2 = st.columns(2)
+c1.markdown(f"""
+    <div style='margin-bottom: 20px; width:100%; background-color: #2d2d2d; padding: 20px 30px; border-radius: 15px; 
+                box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.1);'>
+        <p style='font-size: 16px; font-weight: 600; color: #f9f9f9; margin-bottom: 10px;'>Total Daily Problems Completed: {500}</p>
+        <p style='font-size: 12px; color: #dddddd; margin-bottom: 5px;'>Current Streak: {"10 days"}</p>
+        <p style='font-size: 12px; color: #dddddd;'>Longest Streak: {"30 days"}</p>
+    </div>
+    """, unsafe_allow_html=True)
+
+c2.markdown(f"""
+    <div style='margin-bottom: 20px; width:100%; background-color: #2d2d2d; padding: 20px 30px; border-radius: 15px; 
+                box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.1);'>
+        <p style='font-size: 16px; font-weight: 600; color: #f9f9f9; margin-bottom: 10px;'>Today's Daily Challenge Problem</p>
+        <p style='font-size: 12px; color: #dddddd; margin-bottom: 5px;'>Current Streak: {"10 days"}</p>
+        <p style='font-size: 12px; color: #dddddd;'>Longest Streak: {"30 days"}</p>
+    </div>
+    """, unsafe_allow_html=True)
 
 
 # Expander for filters on top of the chart
@@ -76,8 +99,7 @@ with st.container():
     fig.update_traces(
         mode="markers+lines",
         hovertemplate=(
-            "%{customdata[0]}<br>"
-            "%{customdata[1]}<br>"
+            "%{customdata[0]}. %{customdata[1]}<br>"
             "Date: %{x}<br>"
             "Complete Time: %{y}<br>"
             "Complexity: %{customdata[2]}<br><br>"

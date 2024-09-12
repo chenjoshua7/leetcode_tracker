817be6b3945c8ce7c32c9c4386b8292399056a39
Joshua Chen <chen.joshua98@gmail.com>
2024-09-12 16:15:08 -0400

    with streamlit user on sl

--- streamlit/etl.py
+++ streamlit/etl.py
@@ -1,6 +1,58 @@
 import pandas as pd
 import pymysql
+    
+class DataQuerier:
+    def __init__(self) -> None:
+        self.connection = None
+        self.col_names = None
+        self._connect()
+        self._get_column_names()
+        pass
+    
+    def _connect(self):
+        endpoint = "leetcode.c9eq4wc6mqs0.us-east-2.rds.amazonaws.com"
+        port = 3306
+        username = "streamlit"
+        password = "password123"
+        database = "leetcode"
 
+        try:
+            self.connection = pymysql.connect(
+                host=endpoint,
+                user=username,
+                password=password,
+                database=database,
+                port=port
+            )
+            print("Connection successful!")
+            
+        except pymysql.MySQLError as e:
+            print(f"Error: {e}")
+    
+    def _get_column_names(self):
+        self.col_names = list(self.query("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'daily_problems'")[0])
+    
+    def query(self, query, *args, **kwargs):
+        column_names = kwargs.get("columns_names", None) or kwargs.get("col_names", None) or kwargs.get("columns", None) 
+        try:
+            with self.connection.cursor() as cursor:
+                cursor.execute(query)
+                results = cursor.fetchall()
+                print("Successfully Executed")
+                if column_names:
+                    return pd.DataFrame(results, columns = column_names)
+                else:
+                    return pd.DataFrame(results)
+        except pymysql.MySQLError as e:
+            print(f"Error: {e}")
+    
+    def close(self):
+        self.connection.close()
+        
+      
+
+### my first class for when I was using a local server  
+        
 class ETL:
     def __init__(self) -> None:
         self.df = None
@@ -36,51 +88,3 @@ class ETL:
     
     def export(self):
         return self.df
-    
-class DataQuerier:
-    def __init__(self) -> None:
-        self.connection = None
-        self.col_names = None
-        self._connect()
-        self._get_column_names()
-        pass
-    
-    def _connect(self):
-        endpoint = "leetcode.c9eq4wc6mqs0.us-east-2.rds.amazonaws.com"
-        port = 3306
-        username = "admin"
-        password = "Huahua2010!"
-        database = "leetcode"
-
-        try:
-            self.connection = pymysql.connect(
-                host=endpoint,
-                user=username,
-                password=password,
-                database=database,
-                port=port
-            )
-            print("Connection successful!")
-            
-        except pymysql.MySQLError as e:
-            print(f"Error: {e}")
-    
-    def _get_column_names(self):
-        self.col_names = list(self.query("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'daily_problems'")[0])
-    
-    def query(self, query, *args, **kwargs):
-        column_names = kwargs.get("columns_names", None) or kwargs.get("col_names", None) or kwargs.get("columns", None) 
-        try:
-            with self.connection.cursor() as cursor:
-                cursor.execute(query)
-                results = cursor.fetchall()
-                print("Successfully Executed")
-                if column_names:
-                    return pd.DataFrame(results, columns = column_names)
-                else:
-                    return pd.DataFrame(results)
-        except pymysql.MySQLError as e:
-            print(f"Error: {e}")
-    
-    def close(self):
-        self.connection.close()

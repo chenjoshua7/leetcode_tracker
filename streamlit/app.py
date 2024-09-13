6d30e91646836944c2a906063f7e34b07ac40150
Joshua Chen <chen.joshua98@gmail.com>
2024-09-13 12:00:15 -0400

    wide format

--- streamlit/app.py
+++ streamlit/app.py
@@ -6,9 +6,13 @@ import webbrowser
 from datetime import timedelta
 
 from etl import DataQuerier
-from helper_functions import wrap_text, get_current_streak, get_daily_question
+from helper_functions import wrap_text, get_current_streak, get_daily_question, get_master_query
 from queries import streak_query
 
+# Wide Layout
+st.set_page_config(layout="wide")
+
+# Set Up
 querier = DataQuerier()
 df = querier.query("SELECT * FROM daily_problems ORDER BY date DESC")
 question_count = querier.query("SELECT COUNT(*) FROM daily_problems")
@@ -23,7 +27,18 @@ daily_question = get_daily_question(df)
 
 # Sidebar navigation
 with st.sidebar:
+    with st.expander("Filters", expanded=False):
+        c1, c2= st.columns(2)
+        with st.container():
+            # Date pickers for start and end dates
+            start_date = c1.date_input("Start Date", value=df['date'].min())
+            end_date = c2.date_input("End Date", value=df['date'].max() + timedelta(days=1))
+
+        # Filter by complexity
+        complexity = st.multiselect("Complexity", ['Easy', 'Medium', 'Hard'])
+        
     st.markdown("<h1 style='text-align: center; padding: 20px 0;'>Navigation</h1>", unsafe_allow_html=True)
+    
     
     # Create columns for better alignment of buttons
     col1 = st.columns(1)
@@ -39,17 +54,15 @@ with st.sidebar:
 
     st.markdown("<p style='margin-top: 10px;'>Email: <a href='mailto:chen.joshua98@gmail.com'>chen.joshua98@gmail.com</a></p>", unsafe_allow_html=True)
     st.markdown("<p style='margin-top: 0px;'>Repo: <a href='https://github.com/chenjoshua7/leetcode_tracker'>/chenjoshua7/leetcode_tracker</a></p>", unsafe_allow_html=True)
-
-    st.markdown("<h2>About the Project</h2>", unsafe_allow_html=True)
-    st.markdown("")
+    
 # Title and description
 st.markdown("<h1 style='text-align: center; padding-top:-10px; padding-bottom: 20px;'>LeetCode Daily Challenge Progress</h1>", unsafe_allow_html=True)
 st.markdown("<h4 style='text-align: center; padding-bottom: 10px;'>Joshua Chen</h4>", unsafe_allow_html=True)
 st.markdown("<p style='text-align: justify; font-size: 18px; padding-left: 30px; padding-right: 30px;'>Check out my progress with LeetCode Daily Challenges, where I solve algorithmic problems to hone my problem-solving skills.</p>", unsafe_allow_html=True)
 
-c1, c2 = st.columns([1, 1.2]) 
+space1, c1, space2, c2, space3 = st.columns([0.1,1,0.3, 1.5, 0.1]) 
 c1.markdown(f"""
-    <div style='margin-bottom: 20px; width:100%; background-color: #2d2d2d; padding: 20px 30px 10px 30px; border-radius: 15px; 
+    <div style='text-align:center; margin-bottom: 20px; width:100%; background-color: #2d2d2d; padding: 20px 30px 10px 30px; border-radius: 15px; 
                 box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.1);'>
         <p style='font-size: 16px; font-weight: 600; color: #f9f9f9; margin-bottom: 5px;'>Total Completed: {question_count.iloc[0,0]}</p>
         <p style='font-size: 15px; color: #dddddd; margin-bottom: 5px;'>Current Streak: {current_streak} days</p>
@@ -59,32 +72,11 @@ c1.markdown(f"""
 
 c2.markdown(daily_question, unsafe_allow_html=True)
 
-
-# Expander for filters on top of the chart
-with st.expander("Filters", expanded=False):
-    c1, c2= st.columns(2)
-    with st.container():
-        # Date pickers for start and end dates
-        start_date = c1.date_input("Start Date", value=df['date'].min())
-        end_date = c2.date_input("End Date", value=df['date'].max() + timedelta(days=1))
-    
-    #filter by complexity:
-    complexity = st.multiselect("Complexity", ['Easy', 'Medium', 'Hard'])
-
 if not complexity:
     complexity = ['Easy', 'Medium', 'Hard']
     
-# Ensure dates are in the correct format
-start_date_str = start_date.strftime('%Y-%m-%d')
-end_date_str = end_date.strftime('%Y-%m-%d')
-complexity_str = ', '.join([f"'{c}'" for c in complexity])
-
 # Querying data from filters:
-master_query = f"""
-    SELECT * FROM daily_problems
-    WHERE date BETWEEN '{start_date_str}' AND '{end_date_str}'
-    AND complexity IN ({complexity_str});
-"""
+master_query = get_master_query(start_date, end_date, complexity)
 df = querier.query(master_query)
 
 df_filtered = df[df['time'] >= 60]

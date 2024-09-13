054c93507c53d5c95971afa64af76efa96fbafd5
Joshua Chen <chen.joshua98@gmail.com>
2024-09-13 10:43:08 -0400

    streak query

---
+++ streamlit/queries.py
@@ -0,0 +1,32 @@
+streak_query = f"""WITH ranked_data AS (
+                        SELECT 
+                            *,
+                            LAG(CAST(date AS DATE), 1) OVER (ORDER BY date ASC) AS prev_date
+                        FROM 
+                            daily_problems
+                    ),
+                    streaks AS (
+                        SELECT 
+                            *,
+                            CASE 
+                                WHEN DATEDIFF(CAST(date AS DATE), prev_date) = 1 THEN 0
+                                ELSE 1
+                            END AS is_new_streak
+                        FROM ranked_data
+                    ),
+                    streak_groups AS (
+                        SELECT *,
+                            SUM(is_new_streak) OVER (ORDER BY date DESC ROWS UNBOUNDED PRECEDING) AS streak_id
+                        FROM streaks
+                    )
+                    SELECT 
+                        streak_id,
+                        MAX(date) AS end_date,
+                        COUNT(*) AS streak_length
+                    FROM 
+                        streak_groups
+                    GROUP BY 
+                        streak_id
+                    ORDER BY 
+                        end_date DESC;
+                    """

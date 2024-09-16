6e70c9be4c52a303d794f606d08626504d3a4184
Joshua Chen <chen.joshua98@gmail.com>
2024-09-16 08:37:56 -0400

    gpt-less streak

--- streamlit/queries.py
+++ streamlit/queries.py
@@ -31,3 +31,36 @@ streak_query = f"""WITH ranked_data AS (
                         end_date DESC;
                     """
                   
+streak_gpt_query = f"""WITH ranked_data AS (
+                        SELECT 
+                            *,
+                            LAG(CAST(date AS DATE), 1) OVER (ORDER BY date ASC) AS prev_date
+                        FROM 
+                            daily_problems
+                        WHERE gpt = 0  -- Filter to include only rows where gpt = 0
+                    ),
+                    streaks AS (
+                        SELECT 
+                            *,
+                            CASE 
+                                WHEN DATEDIFF(CAST(date AS DATE), prev_date) = 1 THEN 0
+                                ELSE 1
+                            END AS is_new_streak
+                        FROM ranked_data
+                    ),
+                    streak_groups AS (
+                        SELECT *,
+                            SUM(is_new_streak) OVER (ORDER BY date DESC ROWS UNBOUNDED PRECEDING) AS streak_id
+                        FROM streaks
+                    )
+                    SELECT 
+                        streak_id,
+                        MAX(date) AS end_date,
+                        COUNT(*) AS streak_length
+                    FROM 
+                        streak_groups
+                    GROUP BY 
+                        streak_id
+                    ORDER BY 
+                        end_date DESC;
+                    """
